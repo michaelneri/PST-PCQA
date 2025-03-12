36b73678daf90c562a61f55475b4413cefb08d25
Michael Neri <126862574+michaelneri@users.noreply.github.com>
2025-03-12 11:59:27 +0200

    Update patch_extraction.py

--- patch_extraction.py
+++ patch_extraction.py
@@ -6,13 +6,13 @@ from multiprocessing import Pool
 
 def preprocessing(dataset_npz, pc_path, filename, index):
     # perform preprocessing and save all data
-    x_b, x_s = patches_extraction(filename = pc_path, number_patches = 32, points_per_patch = 14900, small_points_per_patch = 8192)
+    x_b, x_s = patches_extraction(filename = pc_path, number_patches = 16, points_per_patch = 14900, small_points_per_patch = 8192)
     np.savez(os.path.join(dataset_npz, filename), x_b=x_b, x_s=x_s)
     print("{}) {} saved ".format(index, os.path.join(dataset_npz, filename) ))
 
 if __name__ == "__main__":
     dataset_ply_path = "data/WPC/distorted"
-    destination_npz_path = "data/WPC/distorted_npz_32"
+    destination_npz_path = "data/WPC/distorted_npz_16"
     list_pc = [os.path.join(dataset_ply_path, f) for f in os.listdir(dataset_ply_path)]
     filenames = [f.split("\\")[1][:-4] for f in list_pc]
 
